1c322a5503c943b6c1fa875e6f4346372c496785
Hattiwatti <hattiwatti05@gmail.com>
2018-09-22 22:23:19 +0300

    Light stuff

--- ROTTR/Foundation.h
+++ ROTTR/Foundation.h
@@ -4,6 +4,7 @@
 
 namespace Foundation
 {
+
   class GameRender;
   class CommonLightResource;
   class CommonRenderLight;
@@ -33,9 +34,23 @@ namespace Foundation
 
   class CommonLightResource
   {
+    static const unsigned char DefaultVoxelData[0xFF];
+
   public:
-    char Pad000[0x4A0];
-    // TODO: Figure it out because entity variables are not enough
+    char Pad000[0x120];
+    DirectX::XMFLOAT4 m_Color;  // 0x120
+    int m_Type;                 // 0x130
+    char Pad134[0x4];           // 0x134
+    float m_Unk1;               // 0x138 Default 1.0
+    char Pad13C[0x1B];          // 0x13C
+    unsigned char m_VoxelData[0xFF];  // 0x157
+    char Pad256[0x32];          // 0x256
+    int m_Unk2;                 // 0x288 Default 1
+    char Pad28C[0x1A4];         // 0x28C
+    DirectX::XMFLOAT4 m_Unk3;   // 0x430 Default 1 1 1 1, specular etc multipliers?
+    float m_Unk4;               // 0x440 Default 1.0
+    float m_Unk5;               // 0x444 Default 1.0
+    char Pad448[0x58];          // 0x448
   public:
     // Creates a light resource with empty data
     static CommonLightResource* Create()
@@ -47,6 +62,18 @@ namespace Foundation
 
       return CreateLightResource(0, dummyData);
     }
+
+    void LoadDefaultValues()
+    {
+      m_Type = 1;
+      m_Unk1 = 1.f;
+      m_Unk2 = 1;
+      m_Unk3 = { 1.0f, 1.0f, 1.0f, 1.0f };
+      m_Unk4 = 1.0f;
+      m_Unk5 = 1.0f;
+
+      memcpy(&m_VoxelData[0], &DefaultVoxelData[0], 0xFF);
+    }
   }; // Size = 0x4A0
 
   class CommonRenderLight
@@ -55,23 +82,60 @@ namespace Foundation
     char Pad000[0x30];
     SceneLight* m_SceneLight;
     CommonLightResource* m_LightResource;
-    char Pad040[0x20];
+    float m_Unk1;                   // 0x40 Default 3.61
+    float m_Unk2;                   // 0x44 Default -2.509
+    char Pad048[0x10];              // 0x48
+    float m_Unk3;                   // 0x58 Default 1.0f
+    char Pad05C[0x4];               // 0x5C
     DirectX::XMMATRIX m_Transform;  // 0x60
     DirectX::XMFLOAT4 m_Color;      // 0xA0
     float m_AttenuationDistance;    // 0xB0
-    float m_Unk1;       // 0xB4 Loads of spotlight values
-    float m_Unk2;       // 0xB8
-    float m_Unk3;       // 0xBC
-    char Pad0C0[0x24];  // 0xC0
-    float m_Unk4;       // 0xE4
-    float m_Intensity;  // 0xE8
-    float m_Unk5;       // 0xEC
-    char Pad0F0[0x4];   // 0xF0
-    float m_Unk6;       // 0xF4
-    char Pad0F8[0x48];  // 0xF8
-    int m_Flag1;        // 0x140 Type? 0 for point, 1/2 for spot
-    int m_Flag2;        // 0x144 Updateflags?
+    float m_Unk4;       // 0xB4 Default 1.64f
+    float m_Unk5;       // 0xB8 Default 0.61f
+    float m_Unk6;       // 0xBC Default 0
+    DirectX::XMFLOAT4 m_Unk7; // 0xC0 Default 2000 4000 8000 1.0f
+    float m_Unk8;       // 0xD0 Default 2.20
+    char Pad0D4[0xC];   // 0xD4
+    float m_Unk9;       // 0xE0 Default 1.0
+    float m_Unk10;      // 0xE4 Default 2.0
+    float m_Intensity;  // 0xE8 Default 1.0
+    float m_Unk11;      // 0xEC Default 1.0
+    float m_Unk12;      // 0xF0 Default 0
+    float m_Unk13;      // 0xF4 Default 1.07
+    float m_Unk14;      // 0xF8 Default 0
+    float m_Unk15;      // 0xFC Default 0
+    DirectX::XMMATRIX m_Unk16; // 0x100 Default identity
+    int m_Flag1;        // 0x140 Default 2
+    int m_Flag2;        // 0x144 Default 0xC4
     char Pad148[0xB8];  // 0x148
+
+  public:
+    void LoadDefaultValues()
+    {
+      memset(&Pad048[0], 0, 0x10);
+      memset(&Pad05C[0], 0, 0x4);
+      memset(&Pad0D4[0], 0, 0xC);
+
+      m_Unk1 = 3.61f;
+      m_Unk2 = -2.509f;
+      m_Unk3 = 1.0f;
+      m_Unk4 = 1.64f;
+      m_Unk5 = 0.61f;
+      m_Unk6 = 0.f;
+      m_Unk7 = { 2000.f, 4000.f, 8000.f, 1.0f };
+      m_Unk8 = 2.20f;
+      m_Unk9 = 1.0f;
+      m_Unk10 = 2.0f;
+      m_Intensity = 1.0f;
+      m_Unk11 = 1.0f;
+      m_Unk12 = 0.f;
+      m_Unk13 = 1.07f;
+      m_Unk14 = 0.f;
+      m_Unk15 = 0.f;
+      m_Unk16 = DirectX::XMMatrixIdentity();
+      m_Flag1 = 2;
+      m_Flag2 = 0xC4;
+    }
   }; // Size = 0x200
 
   class LightRenderCallback
